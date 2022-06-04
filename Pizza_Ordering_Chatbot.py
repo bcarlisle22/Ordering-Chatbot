02cfdde616d602d271ca803af645c0656379abc4
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-06-04 14:17:59 -0700

    Added data validation

--- Pizza_Ordering_Chatbot.py
+++ Pizza_Ordering_Chatbot.py
@@ -1,18 +1,54 @@
 print('Hello my name is Alex, your virtual assistant. I will help you order a pizza!')
 print('I am going to ask you a few questions. After typing an answer, please press enter.')
-username=input('\n Enter your name:  ')
-if username.lower() == "brianna carlisle": 
-    print("\n That name sounds familiar.")
-else:
-    print("\n Nice to meet you!")
 
-print("\n Well, I know you're hungry, so let's get started! ")
-size=input("\n What size pizza would you like? Please enter small, medium, or large.  ")
-flavor = input('\n Please enter the type of pizza you would like :  ')
-crustType = input('\n Please enter the type of crust you would like :  ')
-quantity = input('\n Please enter the quantity of pizzas you would like. Please enter a numeric value:   ')
+userName=input('Enter your name:  ')
+while(len(userName) == 0):
+    userName = input("Please enter your name: ")
+
+
+if userName.lower() == "brianna carlisle": 
+    print("I love that name!")
+else:
+    print("Nice to meet you!")
+
+print("\n")
+
+print("I know you're hungry, so let's get started! ")
+
+print("\n")
+
+size=input("What size pizza would you like? Please enter small, medium, or large.  ").lower()
+while size!= "small" and size != "medium" and size != "large":
+    size = input("Invalid value. Please enter small, medium, or large: ").lower()
+
+print("\n")
+
+flavor = input('Please enter the type of pizza you would like :  ')
+while(len(flavor) == 0):
+    flavor = input("Please enter a pizza flavor: ")
+
+print("\n")
+
+crustType = input('Please enter the type of crust you would like:  ')
+while(len(crustType) == 0):
+    crustType = input("Please enter pizza crust type: ")
+
+print("\n")
+
+quantity = input('Please enter the quantity of pizzas you would like. Please enter a numeric value:  ')
+while(len(quantity) == 0):
+   quantity = input("Please enter how many pizzas you would like:  ")
 quantity=int(quantity)
-method = input('\n Great! Would this be for carryout or delivery?  ')
+
+print("\n")
+
+method = input('Great! Will this be for carryout or delivery?  ')
+while(len(method) == 0):
+    method = input("Please choose carryout or delivery: ")
+
+
+print("\n")
+
 
 salesTax= 1.1
 if size.lower()== "small":
@@ -26,11 +62,18 @@ if method.lower()== "delivery":
     deliveryFee= 5
 else:
     deliveryFee= 0
-total=(pizzaCost * quantity ) * salesTax + deliveryFee
+
+
+total=( pizzaCost * quantity ) * salesTax + deliveryFee
+
+
 print('\n ------------------------')
-print('\n Thank you, ' + username + ' for your order!') 
-print("\n Your ", quantity, size, flavor + ' pizza with',  crustType, "crust costs:", "${:,.2f}" .format (total) + ".")
-print("\n We are currently preparing your order. It will be ready for " + method + " soon!")
+
+print('Thank you, ' + userName + ' for your order!') 
+print("Your ", quantity, size, flavor + ' pizza with',  crustType, "crust costs:", "${:,.2f}" .format (total) + ".")
+print("We are currently preparing your order. It will be ready for " + method + " soon!")
+
 print('\n')
-print('\n Thank you for using Max, the virtual assistant. See you next time!')
-print('\n ------------------------')
+
+print('Thank you for using Max, the virtual assistant. See you next time!')
+print('------------------------')
